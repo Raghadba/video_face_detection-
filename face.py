25c3a43b99a7d31cf992b562eea82240f0120940
Raghad Baeshen <67527027+Raghadba@users.noreply.github.com>
2022-11-21 18:33:17 +0300

    Update face.py

--- face.py
+++ face.py
@@ -1,6 +1,7 @@
+# Importing OpenCV package
 import cv2
 
-
+# Loading the required haar-cascade xml classifier file
 face_ = cv2.CascadeClassifier('/Users/raghadbaeshen/PycharmProjects/OpencvPython/haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('/Users/raghadbaeshen/PycharmProjects/OpencvPython/haarcascade_eye.xml')
 
@@ -9,9 +10,11 @@ while cap.isOpened():
 
     ret, img = cap.read()
     print(ret)
+    # Converting image to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_.detectMultiScale(gray, 1.3, 5)
-
+    
+# Iterating through rectangles of detected faces
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
         roi_gray = gray[y:y + h, x:x + w]
